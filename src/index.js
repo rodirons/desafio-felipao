bdb8f8cba292e425a6b33adef2ce73199074b286
rodirons <rodirons86@gmail.com>
2023-11-09 20:28:32 +0000

    finalizado

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("oi");
+
+let nomeDoHeroi = "Rodnelson";
+let experiencia = 7511;
+
+let nivelDoHeroi;
+
+if (experiencia < 1000) {
+    nivelDoHeroi = "Ferro";
+} else if (experiencia > 1000 && experiencia <= 2000) {
+    nivelDoHeroi = "Bronze";
+} else if (experiencia > 2000 && experiencia <= 5000) {
+    nivelDoHeroi = "Prata";
+} else if (experiencia > 5000 && experiencia <= 7000) {
+    nivelDoHeroi = "Ouro";
+} else if (experiencia > 7000 && experiencia <= 8000) {
+    nivelDoHeroi = "Platina";
+} else if (experiencia > 8000 && experiencia <= 9000) {
+    nivelDoHeroi = "Ascendente";
+} else if (experiencia > 9000 && experiencia <= 10000) {
+    nivelDoHeroi = "Imortal";
+} else {
+    nivelDoHeroi = "Radiante";
+}
+
+console.log("O Herói " + nomeDoHeroi + " está no nível " + nivelDoHeroi + ".");
